ab34680aaa4933b3d8cda138cadfa4846ad81bfc
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2026-08-12 00:08:16 -0500

    feat: track game scores and winning rounds

--- src/components/Gameboard/Gameboard.jsx
+++ src/components/Gameboard/Gameboard.jsx
@@ -5,8 +5,11 @@ import shuffle from "../../scripts/shuffle";
 import Scoreboard from "../Scoreboard/Scoreboard";
 
 export default function Gameboard() {
+  // TODO: Implement loading and failed to load functionality
   const [gameCards, setGameCards] = useState([]);
   const [clickedCards, setClickedCards] = useState([]);
+  const [bestScore, setBestScore] = useState(0);
+  const score = clickedCards.length;
 
   useEffect(() => {
     (async () => {
@@ -16,18 +19,28 @@ export default function Gameboard() {
   }, []);
 
   const handleGameCardClick = (pokemonId) => {
-    console.log(pokemonId);
-    clickedCards.includes(pokemonId)
-      ? console.log("Gameover")
-      : setClickedCards([...clickedCards, pokemonId]);
-    console.log(clickedCards.length);
-    console.log(clickedCards);
-    setGameCards(shuffle(gameCards));
+    if (clickedCards.includes(pokemonId)) {
+      setClickedCards([]);
+      // TODO: Implement Duplicate Pokemon - Team Rocket Has Deceived You
+    } else {
+      const newClickedCards = [...clickedCards, pokemonId];
+      const newScore = newClickedCards.length;
+
+      setClickedCards(newClickedCards);
+      setBestScore((previousBest) => Math.max(previousBest, newScore));
+
+      if (newScore === gameCards.length) {
+        // TODO: Add New Game And You Win Form
+        alert("You Win");
+      }
+    }
+
+    setGameCards((cards) => shuffle(cards));
   };
 
   return (
     <main className="gameboard">
-      <Scoreboard />
+      <Scoreboard score={score} bestScore={bestScore} />
       {gameCards.map((pokemon) => (
         <Card
           key={pokemon.id}
